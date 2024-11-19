6849cb8c0a871ee710d35d86642c2c9d94ccb040
Raigorx Hellscream <10247765+raigorx@users.noreply.github.com>
2024-11-19 08:05:46 -0400

    Remove /.history directory

--- .history/resources/views/products/index.blade_20241110060948.php
+++
@@ -1,5 +0,0 @@
-@extends('layout.app')
-
-@section('title', 'Products list')
-
-@section('content')
--- .history/resources/views/products/index.blade_20241110061001.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.app')
-
-@section('title', 'Products list')
-
-@section('content')
-
-@endsection
